3136818007317d982826909687bdc6ae9df7c6b5
xiangxin19960319 <xiangxin19960319@gmail.com>
2014-07-22 00:11:01 +0800

    Fix build modem include phone.h error

--- include/linux/phonet.h
+++ include/linux/phonet.h
@@ -99,11 +99,11 @@ struct phonetmsg {
 
 /* Phonet socket address structure */
 struct sockaddr_pn {
-	__kernel_sa_family_t spn_family;
+	sa_family_t spn_family;
 	__u8 spn_obj;
 	__u8 spn_dev;
 	__u8 spn_resource;
-	__u8 spn_zero[sizeof(struct sockaddr) - sizeof(__kernel_sa_family_t) - 3];
+	__u8 spn_zero[sizeof(struct sockaddr) - sizeof(sa_family_t) - 3];
 } __attribute__((packed));
 
 /* Well known address */
