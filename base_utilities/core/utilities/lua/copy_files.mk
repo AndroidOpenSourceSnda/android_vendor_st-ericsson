0be3488e9e301b1977d0665308a47ad05d2d2e58
xiangxin19960319 <xiangxin19960319@gmail.com>
2014-08-03 22:52:16 +0800

    Clean code

--- base_utilities/core/utilities/lua/copy_files.mk
+++
@@ -1,2 +0,0 @@
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/static/lua:/data/lua/bin/lua
-PRODUCT_COPY_FILES += $(LOCAL_PATH)/setup.sh:/data/lua/setup.sh
--- base_utilities/core/utilities/lua/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /data/lua/bin
-
-chmod 0777 ./lua
-
-export PATH=/data/lua/bin:$PATH
-
-cd -
