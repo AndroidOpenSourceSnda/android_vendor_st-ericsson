a2dbfb4019e6592fd6ef15d79f130b9364e73a73
xiangxin19960319 <xiangxin19960319@gmail.com>
2014-08-04 20:10:27 +0800

    update u8500/vendorsetup.sh

--- u8500/vendorsetup.sh
+++ u8500/vendorsetup.sh
@@ -1,6 +1,6 @@
 T=$(gettop)
 if [ -e $T/vendor/st-ericsson/u8500/BoardConfig.mk ]; then
-  add_lunch_combo s1w_u8500-eng
+  add_lunch_combo s1w_u8500-userdebug
   add_lunch_combo s1w_u8500-user
 fi
 
