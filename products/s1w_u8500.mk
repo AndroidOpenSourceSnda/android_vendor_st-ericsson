7d4ffbcbdfdc33f77722242e9d491a267de32464
xiangxin19960319 <xiangxin19960319@gmail.com>
2014-08-05 16:27:11 +0800

    update s1w_u8500.mk

--- products/s1w_u8500.mk
+++ products/s1w_u8500.mk
@@ -4,6 +4,9 @@ $(call inherit-product, vendor/st-ericsson/products/u8500.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
+# call dalvik heap config
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
 # Overrides
 PRODUCT_BRAND := Bambookphone
 PRODUCT_NAME := s1w_u8500
